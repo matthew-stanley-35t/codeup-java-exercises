4b5c74c199b79330a4e107e678024f80a4e1103c
Matthew Stanley <matthew.stanley35t@gmail.com>
2023-01-02 11:15:03 -0700

    adding Syntax,Types, and Variables exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,41 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "I am a stick!";
+        System.out.println(myString);
+
+        long myNumber = 123;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+        int x = 4;
+        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+        System.out.println(x);
+//        System.out.println(y);
     }
 }
